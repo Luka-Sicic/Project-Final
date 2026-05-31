5ac43c7233040e42f5614416d9a9ede02ee9d8b9
Luka-Sicic <147083282+Luka-Sicic@users.noreply.github.com>
2026-05-31 06:46:53 +0200

    Fix-3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,26 +7,30 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
 
     Vector2 moveDirection;
-    Vector2 mousePosition;
+    Vector2 mousePosition; // This will now correctly store the value for FixedUpdate
 
     void Update()
     {
+        // 1. Gather Input
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
+        moveDirection = new Vector2(moveX, moveY).normalized;
 
         if (Input.GetMouseButtonDown(0))
             gun.Fire();
 
-        moveDirection = new Vector2(moveX, moveY).normalized;
-
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 lookDirection = mousePosition - rb.position;
-        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
-        rb.MoveRotation(angle);
+        // 2. Sample Mouse Position (Remove 'Vector2' here to fix variable shadowing)
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void FixedUpdate()
     {
+        // 3. Apply Velocity
         rb.linearVelocity = moveDirection * moveSpeed;
+
+        // 4. Apply Rotation in FixedUpdate
+        Vector2 lookDirection = mousePosition - rb.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        rb.MoveRotation(angle);
     }
 }

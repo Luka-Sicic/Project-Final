322ff3a660b2ac5ca091d2398bd0b2703e611520
Luka-Sicic <147083282+Luka-Sicic@users.noreply.github.com>
2026-06-05 23:15:00 +0200

    Fix-20

--- Assets/Scripts/Bat.cs
+++ Assets/Scripts/Bat.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using Project.Scripts;
 
 public class Bat : Weapon
 {
@@ -24,7 +25,31 @@ public class Bat : Weapon
         }
 
         // Melee logic
-        Debug.Log("Bat swung!");
-        // For now, just a log. Collision logic can be added later.
+        Vector3 point = attackPoint != null ? attackPoint.position : transform.position;
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(point, attackRange, enemyLayers);
+
+        Debug.Log($"Bat swung at {point}. Found {hitEnemies.Length} colliders on layers {enemyLayers.value}");
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Health health = enemy.GetComponent<Health>();
+            if (health != null)
+            {
+                Debug.Log($"Hit enemy: {enemy.name}");
+                health.TakeDamage(1);
+            }
+            else
+            {
+                Debug.Log($"Hit object {enemy.name} but it has no Health component.");
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Vector3 point = attackPoint != null ? attackPoint.position : transform.position;
+        Gizmos.DrawWireSphere(point, attackRange);
     }
 }
+
